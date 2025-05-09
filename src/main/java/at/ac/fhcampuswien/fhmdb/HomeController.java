941248599838a2f2d8dfb35103d50ed647d1f11e
josipzivkovic123 <josip-zivkovic@gmx.at>
2025-05-09 20:10:31 +0200

    fixed the homecontroller

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -2,12 +2,15 @@ package at.ac.fhcampuswien.fhmdb;
 
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -25,21 +28,41 @@ public class HomeController {
 
     private boolean isMenuCollapsed = true;
 
+    private HamburgerBasicCloseTransition transition;
+
     public void initialize(){
         initHamburger();
         setContent("MovieList.fxml");
     }
 
     private void initHamburger() {
-        hamburgerNav.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> toggleMenuDrawer());
+        transition = new HamburgerBasicCloseTransition(hamburgerNav);
+        transition.setRate(-1);
+
+        hamburgerNav.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            toggleHamburgerTransitionState();
+            toggleMenuDrawer();
+        });
+
+        drawer.toBack();
+    }
+
+    private void toggleHamburgerTransitionState() {
+        transition.setRate(transition.getRate() * -1);
+        transition.play();
     }
 
     public void toggleMenuDrawer() {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.3), drawer);
         if (isMenuCollapsed) {
-            drawer.open();
+            transition.setByX(130);
+            drawer.toFront();
         } else {
-            drawer.close();
+            transition.setByX(-130);
+            // Nach Animation im Hintergrund
+            transition.setOnFinished(e -> drawer.toBack());
         }
+        transition.play();
         isMenuCollapsed = !isMenuCollapsed;
     }
 
@@ -59,14 +82,13 @@ public class HomeController {
         }
     }
 
-
     @FXML
-    public void navigateToMovielist(ActionEvent event) {
-
+    public void navigateToMovielist() {
+        setContent("MovieList.fxml");
     }
 
     @FXML
-    public void navigateToWatchlist(ActionEvent event) {
-
+    public void navigateToWatchlist() {
+        setContent("WatchList.fxml");
     }
 }
