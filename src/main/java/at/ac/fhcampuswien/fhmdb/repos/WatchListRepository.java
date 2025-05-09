212d7369b4d4ac9e4bcdeb3e268fd94cad191930
ms-0404 <manuel.schinnerl@stud.fh-campuswien.ac.at>
2025-05-09 19:35:31 +0200

    removed unnec imports

--- src/main/java/at/ac/fhcampuswien/fhmdb/repos/WatchListRepository.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/repos/WatchListRepository.java
@@ -1,11 +1,8 @@
 package at.ac.fhcampuswien.fhmdb.repos;
 
 import at.ac.fhcampuswien.fhmdb.database.DatabaseManager;
-import at.ac.fhcampuswien.fhmdb.database.MovieEntity;
 import at.ac.fhcampuswien.fhmdb.database.WatchlistMovieEntity;
-import at.ac.fhcampuswien.fhmdb.models.Movie;
 import com.j256.ormlite.dao.Dao;
-import com.j256.ormlite.dao.DaoManager;
 
 import java.sql.SQLException;
 import java.util.List;
